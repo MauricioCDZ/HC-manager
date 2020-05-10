967d98d5b0f0049418fdf00f4b0ca09f305c9b80
jeffrey12cali <jeffrey12cali@javerianacali.edu.co>
2020-05-09 19:19:24 -0500

    Base code

---
+++ login/views.py
@@ -0,0 +1,34 @@
+"""
+Archivo donde se enlazan las acciones y eventos del código con las 
+vistas en html.
+"""
+
+from django.shortcuts import render
+import pyrebase
+
+# Configuración del wrap pyrebase
+config = {
+	    'apiKey': "AIzaSyBeSa2PSyHEtt9jPRYZXjRP4myOvGBXoGc",
+	    'authDomain': "hc-manager-e2356.firebaseapp.com",
+	    'databaseURL': "https://hc-manager-e2356.firebaseio.com",
+	    'projectId': "hc-manager-e2356",
+	    'storageBucket': "hc-manager-e2356.appspot.com",
+	    'messagingSenderId': "869491009077",
+	    'appId': "1:869491009077:web:35468c3d8127791a55337c",
+	    'measurementId': "G-C33NC6SJCB"
+}
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
+# Documentar esto mejor -- Esta función retorna la vista de inicio de sesión
+def signIn(request):
+	return  render(request, "login/signIn.html")
+
+
+# Esta función se encarga de captar el email y la contraseña y procede 
+# a realizar  el inicio se sesión através de pyrebase y retorna la vista
+# correspondiente
+def postSign(request):
+	email = request.POST.get('email')
+	user = auth.sign_in_with_email_and_password(email,request.POST.get('pass'))
+	return render(request, "login/welcome.html", {'e':email})
