e79530d4951c07775fb990ca21d99a52e2e1f133
unknown <jeffrey12cali@javerianacali.edu.co>
2020-11-28 18:43:25 -0500

    Pruebas de cobertura

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,6 +1,7 @@
 from django.test import RequestFactory, TestCase
 from django.urls import reverse
 import hc_manager.views as views
+import login.views as lviews
 
 
 class TestViews(TestCase):
@@ -25,17 +26,18 @@ class TestViews(TestCase):
       "fecha" : "29-05-2020-07:34:52",
       "valores" : [ [ "nombre", "naruto" ], [ "edad", "12" ], [ "direccion", "12001 Chalon Rd" ], [ "sexo" ], [ "cedula", "465789123" ], [ "ips", [ "ips", "coopetran" ] ], [ "creacion", "29-05-2020-07:34:52" ], [ "modificacion", "29-05-2020-07:34:52" ], [ "citas" ], "" ]
     }}}
-        self.user_new = {'nombre':'test', 'edad':'15', 'direccion':'test', 'sexo':'masculino', 'cedula':'111111', 'ips' : ''}
-        self.user_new_ips = {'nombre':'test_ips', 'edad':'15', 'direccion':'test', 'sexo':'masculino', 'cedula':'222222', 'ips' : 'test_ips'}
+        self.user_new = {'nombre':'test_ips', 'edad':'15', 'direccion':'test', 'sexo':'masculino', 'cedula':'222222', 'ips' : 'test_ips'}
         self.cita_new = {'peso':'69','estatura':'170','actividad':'frecuente','dieta':'papa','enfermedades':'no','valoracion':'positivo','motivo':'test','comentario':'test'}
 
     def test_post_ips_newHC(self):
-        request = self.factory.post('newHC/', self.user_new_ips)
+        request = self.factory.post('newHC/', self.user_new)
         response = views.newHC(request)
         assert response.status_code == 200
 
     def test_post_newHC(self):
-        request = self.factory.post('newHC/', self.user_new)
+        user = self.user_new
+        user['ips'] = ''
+        request = self.factory.post('newHC/', user)
         response = views.newHC(request)
         assert response.status_code == 200
 
@@ -78,4 +80,118 @@ class TestViews(TestCase):
         path = reverse('app-newCita', kwargs={'cedula': '465789123'})
         request = self.factory.get(path)
         response = views.newCita(request, cedula='465789123')
+        assert response.status_code == 200
+
+    def test_post_search_adminHC(self):
+        request = self.factory.post('/adminHC/', {'cedula_search':'465789123'})
+        response = views.adminHC(request)
+        assert response.status_code == 200
+
+    def test_post_login_adminHC(self):
+        request = self.factory.post('/adminHC/', {'email':'admin@jmail.com', 'pass':'test123'})
+        response = views.adminHC(request)
+        assert response.status_code == 200
+
+    def test_admin(self):
+        path = reverse('app-admin')
+        request = self.factory.get(path)
+        response = views.admin(request)
+        assert response.status_code == 200
+
+    def test_get_adminUser(self):
+        path = reverse('app-adminUser')
+        request = self.factory.get(path)
+        response = views.adminUser(request)
+        assert response.status_code == 200
+
+    def test_post_adminUser(self):
+        request = self.factory.post('/adminUser/', {'email':'admin2@jmail.com', 'pass':'test123'})
+        response = views.adminUser(request)
+        assert response.status_code == 200
+
+    def test_post_ips_adminCreate(self):
+        request = self.factory.post('/adminCreate/', self.user_new)
+        response = views.adminCreate(request)
+        assert response.status_code == 200
+
+    def test_post_no_ips_adminCreate(self):
+        user = self.user_new
+        user['ips'] = ''
+        request = self.factory.post('/adminCreate/', user)
+        response = views.adminCreate(request)
+        assert response.status_code == 200
+
+    def test_get_adminCreate(self):
+        path = reverse('app-adminCreate')
+        request = self.factory.get(path)
+        response = views.adminCreate(request)
+        assert response.status_code == 200
+
+    def test_post_ips_adminEdit(self):
+        request = self.factory.post('/adminEdit/465789123', self.user_legit)
+        response = views.adminEdit(request, cedula='465789123')
+        assert response.status_code == 200
+
+    def test_post_no_ips_adminEdit(self):
+        user = self.user_legit
+        user['ips'] = ''
+        request = self.factory.post('/adminEdit/465789123', user)
+        response = views.adminEdit(request, cedula='465789123')
+        assert response.status_code == 200
+
+    def test_get_adminEdit(self):
+        path = reverse('app-adminEdit', kwargs={'cedula': '465789123'})
+        request = self.factory.get(path)
+        response = views.adminEdit(request, cedula='465789123')
+        assert response.status_code == 200
+
+    def test_get_adminLog(self):
+        path = reverse('app-adminLog', kwargs={'cedula': '465789123'})
+        request = self.factory.get(path)
+        response = views.adminLog(request, cedula='465789123')
+        assert response.status_code == 200
+
+    def test_post_adminCita(self):
+        request = self.factory.post('/adminCita/465789123', self.cita_new)
+        response = views.adminCita(request, cedula='465789123')
+        assert response.status_code == 200
+
+    def test_get_adminCita(self):
+        path = reverse('app-adminCita', kwargs={'cedula': '465789123'})
+        request = self.factory.get(path)
+        response = views.adminCita(request, cedula='465789123')
+        assert response.status_code == 200
+
+    def test_get_adminDelHC(self):
+        path = reverse('app-adminDelHC', kwargs={'cedula': '222222'})
+        request = self.factory.get(path)
+        response = views.adminDelHC(request, cedula='222222')
+        assert response.status_code == 200
+
+    def test_get_help(self):
+        path = reverse('app-help')
+        request = self.factory.get(path)
+        response = views.help(request)
+        assert response.status_code == 200
+
+    def test_get_signIn(self):
+        path = reverse('login-signIn')
+        request = self.factory.get(path)
+        response = lviews.signIn(request)
+        assert response.status_code == 200
+
+    def test_post_search_postSign(self):
+        request = self.factory.post('/postSign/', {'email':'gargal@gmail.com', 'pass':'test123'})
+        response = lviews.postSign(request)
+        assert response.status_code == 200
+
+    def test_post_login_postSign(self):
+        request = self.factory.post('/postSign/', {'cedula_search':'465789123'})
+        response = lviews.postSign(request)
+        assert response.status_code == 200
+
+    def test_get_postSign(self):
+        path = reverse('login-welcome')
+        request = self.factory.get(path)
+        response = lviews.postSign(request)
         assert response.status_code == 200
